e858248f3971cba033a9ad19fad128e56e1bc23a
DMu1e <daltonmulem@gmail.com>
2025-04-26 22:50:52 +0300

    Update Reverse_string.py

--- Reverse_string.py
+++ Reverse_string.py
@@ -3,8 +3,9 @@ x=input("Enter a word:")
 
 reversed_x = ""
 
-i = len(x) - 1  # Start from the last character
+i = len(x) - 1  # Starts writing from the last character
 
+# The loop writes every individual letter until the full word is formed
 while i >= 0:
     reversed_x += x[i]
     i -= 1
