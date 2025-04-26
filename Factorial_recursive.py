bfe6d5aecb8825c64bfd282666e90c6fac048f7f
DMu1e <daltonmulem@gmail.com>
2025-04-26 22:32:12 +0300

    Update Factorial_recursive.py
    adding a way to input and output data

--- Factorial_recursive.py
+++ Factorial_recursive.py
@@ -1,6 +1,12 @@
 #This is code to find the factorial of a number using recursion
-def fact (n):
-    if n==0:
-        return 1
-    return n * fact(n-1)
+x=int(input("Enter the you want the factorial for: "))
 
+# The below function calls upon itself until it reaches the base case where it now send back the info to get the answer
+def fact (n):
+    if n==0: #Base Case 
+        return 1
+    return n * fact(n-1) #Recursive case
+
+ans= fact (x)
+
+print(ans,"is the factorial of",x)
